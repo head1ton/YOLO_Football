b6e922d18c82fdfb9fe29bf27f894a63899a5475
hwan <head1ton@gmail.com>
2025-03-17 11:49:41 +0900

    players, ball tracker

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
 
-    save_video(video_frames, 'output_videos/output_video.mp4')
+    # Draw output
+    ## Draw object Tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
+    save_video(output_video_frames, 'output_videos/output_video.mp4')
 
 if __name__ == "__main__":
     main()
